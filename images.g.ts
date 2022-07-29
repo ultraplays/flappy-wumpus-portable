3b306ff9798fd8cafd7b7a3276a565b5bd7a4b2d
ultraplays <84923630+ultraplays@users.noreply.github.com>
2022-07-29 13:28:13 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "cursor":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . f f f f f f f f f f . . . 
+. . f f f f f f f f f f f f . . 
+. f f f f f f f f f f f f f f . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image1":
+            case "wumpus":return img`
+. . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . 
+. . . . 9 9 9 9 9 9 9 9 9 . . 
+. . . 9 9 9 9 9 9 9 9 9 9 9 . 
+. . . 9 9 9 9 9 9 9 1 1 1 1 1 
+9 . . 9 9 9 9 f f 9 1 1 1 1 1 
+9 . . 9 9 9 9 f f 9 1 f 1 f 1 
+9 . 9 9 9 9 9 9 9 9 1 1 1 1 1 
+. 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
+. 9 9 9 9 9 9 9 9 9 9 9 9 9 . 
+. 9 9 9 9 9 9 9 9 9 9 9 9 . . 
+. 9 9 9 9 9 9 9 9 9 9 . . . . 
+. 9 9 9 9 9 9 9 9 9 9 . . . . 
+. 9 9 9 9 9 9 9 9 9 9 . . . . 
+. 9 . . 9 . . 9 . . 9 . . . . 
+. 9 . . 9 . . 9 . . 9 . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
